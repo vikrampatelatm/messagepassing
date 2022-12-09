d6e7799d5799b4e2d348773a021e7d92e1d79a13
vikrampatelatm <107579052+vikrampatelatm@users.noreply.github.com>
2022-12-09 13:16:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+/*document.querySelector("#message-input").addEventListener("keydown", (event) => {
+    if(event.key === "Enter")
+      getMessage();
+  });
+  
+  document.querySelector("#submit").addEventListener("click", () => {
+      getMessage();
+  });
+  
+  getMessage = () => {
+    document.querySelector("#message-output").textContent = document.querySelector("#message-input").value;
+    document.querySelector("#message-input").value = "";
+  }
+*/
+document.querySelector("#message-input").addEventListener("keydown",(event)=>{
+  if(event.key ==="Enter")
+  getmessage();
+});
+
+document.querySelector("#submit").addEventListener("click",()=>{
+  getmessage();
+});
+getmessage=()=>{
+  document.querySelector("#message-output").textContent = document.querySelector("#message-input").value;
+  document.querySelector("#message-input").value="";
+}
